687d6db9a4a5ff410177c6f4f870c51a93943880
ella-jones <ellajonespriv@gmail.com>
2023-07-15 18:48:01 +0800

    Add dogs for seeding tables

--- src/controllers/cli_controller.py
+++ src/controllers/cli_controller.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 from init import db, bcrypt
 from models.user import User
 from models.breed import Breed
+from models.dog import Dog
 
 db_commands = Blueprint('db', __name__)
 
@@ -32,7 +33,6 @@ def seed_db():
     ]
 
     db.session.add_all(users)
-    db.session.commit()
 
     breeds = [
         Breed(
@@ -54,6 +54,39 @@ def seed_db():
 
     db.session.add_all(breeds)
 
+    dogs = [
+        Dog (
+            name='Dog 1',
+            age='3 and a half',
+            breed=breeds[0],
+            gender='Female',
+            description='Dog 1 desc'
+        ),
+        Dog (
+            name='Dog 2',
+            age='5',
+            breed=breeds[1],
+            gender='Male',
+            description='Dog 2 desc',
+            is_adopted=True
+        ),
+        Dog (
+            name='Dog 3',
+            age='1',
+            breed=breeds[0],
+            gender='Male',
+            description='Dog 3 desc'
+        ),
+        Dog (
+            name='Dog 4',
+            age='4',
+            breed=breeds[2],
+            gender='Female',
+            description='Dog 4 desc'
+        )
+    ]
+
+    db.session.add_all(dogs)
     db.session.commit()
 
     print("Tables Seeded")
