1ccd5d8da76d78782c60682324de4d4dcf2bcd9d
ella-jones <ellajonespriv@gmail.com>
2023-07-15 17:11:52 +0800

    Add Breed Model

---
+++ src/models/breed.py
@@ -0,0 +1,15 @@
+from init import db, ma
+
+class Breed(db.Model):
+    __tablename__ = "breeds"
+
+    id = db.Column(db.Integer, primary_key=True)
+    breed_name = db.Column(db.String(100))
+
+class BreedSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'breed_name')
+        ordered = True
+
+breed_schema = BreedSchema()
+breeds_schema = BreedSchema(many=True)
