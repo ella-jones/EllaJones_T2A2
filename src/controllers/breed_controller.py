819713518b4a7bda6a5bd06dbbeb38d8243fb441
ella-jones <ellajonespriv@gmail.com>
2023-07-20 20:05:41 +0800

    Add @authorise_as_employee decorator to breed_controller routes

--- src/controllers/breed_controller.py
+++ src/controllers/breed_controller.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request
 from init import db
 from models.breed import Breed, breeds_schema, breed_schema
 from flask_jwt_extended import jwt_required
+from controllers.dog_controller import authorise_as_employee
 
 breeds_bp = Blueprint('breeds', __name__, url_prefix='/breeds')
 
@@ -21,6 +22,7 @@ def get_one_breed(id):
 
 @breeds_bp.route('/', methods=['POST'])
 @jwt_required()
+@authorise_as_employee
 def create_breed():
     body_data = breed_schema.load(request.get_json())
     # create a new Breed model instance
@@ -31,20 +33,22 @@ def create_breed():
     db.session.commit()
     return breed_schema.dump(breed), 201
 
-@breeds_bp.route('/<int:id>', methods=['DELETE'])
-@jwt_required()
-def delete_one_breed(id):
-    stmt = db.select(Breed).filter_by(id=id)
-    breed = db.session.scalar(stmt)
-    if breed:
-        db.session.delete(breed)
-        db.session.commit()
-        return {'message': f'Breed: {breed.breed_name} deleted successfully'}
-    else:
-        return {'error': f'Breed not found with id {id}'}, 404
+# @breeds_bp.route('/<int:id>', methods=['DELETE'])
+# @jwt_required()
+# @authorise_as_employee
+# def delete_one_breed(id):
+#     stmt = db.select(Breed).filter_by(id=id)
+#     breed = db.session.scalar(stmt)
+#     if breed:
+#         db.session.delete(breed)
+#         db.session.commit()
+#         return {'message': f'Breed: {breed.breed_name} deleted successfully'}
+#     else:
+#         return {'error': f'Breed not found with id {id}'}, 404
 
 @breeds_bp.route('/<int:id>', methods=['PUT', 'PATCH'])
 @jwt_required()
+@authorise_as_employee
 def update_one_breed(id):
     body_data = breed_schema.load(request.get_json(), partial=True)
     stmt = db.select(Breed).filter_by(id=id)
