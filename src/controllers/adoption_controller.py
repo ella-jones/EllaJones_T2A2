7ecd1c28c2fdde015ee99a6b483dbd82b5dcee63
ella-jones <ellajonespriv@gmail.com>
2023-07-29 15:19:20 +0800

    Add endpoint comments to adoption and auth controllers

--- src/controllers/adoption_controller.py
+++ src/controllers/adoption_controller.py
@@ -11,18 +11,17 @@ adoptions_bp = Blueprint('adoptions', __name__, url_prefix='/adoptions')
 
 @adoptions_bp.route('/') # Method = GET
 def get_all_adoptions():
-    # Gets all adoptions and displays them as a list.
     # No body data input expected from user
     stmt = db.select(Adoption)
     adoptions = db.session.scalars(stmt)
     return adoptions_schema.dump(adoptions)
 
+# The above route gets all adoptions and displays them as a list.
 # expected data returned = all adoption records
 
 
 @adoptions_bp.route('/<int:id>') # Method = Get
 def get_one_adoption(id):
-    # Gets adoption with a specific id
     # No body data input expected from user
     stmt = db.select(Adoption).filter_by(id=id)
     adoption = db.session.scalar(stmt)
@@ -30,23 +29,22 @@ def get_one_adoption(id):
         return adoption_schema.dump(adoption)
     else: return {'error': f'Adoption not found with id {id}'}, 404
 
+# The above route gets adoption with a specific id
 # expected data return = adoption record for adoption with id listed in route (/<int:id>)
+# If no adoption exists with that id, an error message "Adoption not found with id {id}" will be printed.
 
 @adoptions_bp.route('/', methods=['POST']) # Method = Post
-@jwt_required()
-# user must be an employee in order to create (post) adoptions
-@authorise_as_employee
+@jwt_required() # this requires a user to be logged in in order to create (post) adoptions
+@authorise_as_employee # this requires users to be an employee in order to create (post) adoptions
 def create_adoption():
     try:
-        # Check for body data inputted by user (schema.load allows for validation)
         # expected input:
         # {
         #   "dog_id" = 1
         #   "adopter_id" = 1
         #   "notes" = "Adoption 1 Notes"
         # }
-        body_data = adoption_schema.load(request.get_json(), unknown=INCLUDE)
-        # create a new Adoption model instance
+        body_data = adoption_schema.load(request.get_json(), unknown=INCLUDE) # Checks for body data inputted by user (schema.load allows for validation to occur)
         adoption = Adoption(
             dog_id=body_data.get('dog_id'),
             adopter_id=body_data.get('adopter_id'),
@@ -55,55 +53,51 @@ def create_adoption():
             adoption_date=body_data.get('adoption_date'),
             notes=body_data.get('notes') 
         )
-        db.session.add(adoption)
-        db.session.commit()
-        return adoption_schema.dump(adoption), 201
+        db.session.add(adoption) # Adds the adoption to the session
+        db.session.commit() # Commit to add the adoption to the database
+        return adoption_schema.dump(adoption), 201 # Respond to the client
     except IntegrityError as err:
         if err.orig.pgcode == errorcodes.NOT_NULL_VIOLATION:
             return {'error': f'The {err.orig.diag.column_name} is required'}, 409
         if err.orig.pgcode == errorcodes.UNIQUE_VIOLATION:
             return {'error': 'An adoption record for that dog already exists'}, 409
 
-# expected data return = the new adoption record that was just created
-# will return the dog and adopter info nested in the adoption record
+# The above route creates a new Adoption model instance
+# expected data return = the new adoption record that was just created + will return the dog and adopter info nested in the adoption record
 # If a dog_id or adopter_id that does not exist is used, a specific validation error will be thrown.
 # If a user attemps to create an adoption for a dog that has already been adopted, a Unique violation error will be thrown.
 # If the user forgets to input either a dog_id or adopter_id, a Not Null Violation will be thrown.
 
 @adoptions_bp.route('/<int:id>', methods=['DELETE']) # Method = Delete
-@jwt_required()
-# user must be an employee in order to delete adoptions
-@authorise_as_employee
+@jwt_required() # user must be logged in in order to delete adoptions
+@authorise_as_employee # user must be an employee in order to delete adoptions
 def delete_one_adoption(id):
-    # Delete adoption with a specific id
     # No body data input expected from user
     stmt = db.select(Adoption).filter_by(id=id)
     adoption = db.session.scalar(stmt)
     if adoption:
-        db.session.delete(adoption)
-        db.session.commit()
-        return {'message': f'Adoption with {id} was deleted successfully'}
+        db.session.delete(adoption) # Deletes the adoption from the session
+        db.session.commit() # Commit to delete the adoption from the database
+        return {'message': f'Adoption with {id} was deleted successfully'} # Respond to the client
     else:
         return {'error': f'Adoption not found with id {id}'}, 404
     
+# The above route will delete the adoption with the id in the route
 # expected data return = "Adoption with {id} was deleted successfully"
 # If the adoption does not exist, the returned data will be "Adoption not found with id {id}"
-
     
 @adoptions_bp.route('/<int:id>', methods=['PUT', 'PATCH']) 
 # Method used is 'PATCH', however, if the user tries 'PUT', it will return the same function.
-@jwt_required()
-@authorise_as_employee
+@jwt_required() # user must be logged in in order to delete adoptions
+@authorise_as_employee # user must be an employee in order to delete adoptions
 def update_one_adoption(id):
-    # Check for body data inputted by user (schema.load allows for validation)
-    # expected input:
+    # expected input: (any of the following)
     # {
     #   "dog_id" = 1
     #   "adopter_id" = 1
     #   "notes" = "Adoption 1 Notes"
     # }
-    body_data = adoption_schema.load(request.get_json(), unknown=INCLUDE, partial=True)
-    # Update adoption with a specific id
+    body_data = adoption_schema.load(request.get_json(), unknown=INCLUDE, partial=True) # Check for body data inputted by user (schema.load allows for validation)
     stmt = db.select(Adoption).filter_by(id=id)
     adoption = db.session.scalar(stmt)
     if adoption:
@@ -111,12 +105,12 @@ def update_one_adoption(id):
         adoption.adopter_id = body_data.get('adopter_id') or adoption.adopter_id
         adoption.adoption_date = body_data.get('adoption_date') or adoption.adoption_date
         adoption.notes = body_data.get('notes') or adoption.notes
-        db.session.commit()
-        return adoption_schema.dump(adoption)
+        db.session.commit() # Commit to add the changes for the adoption to the database
+        return adoption_schema.dump(adoption) # Respond to the client
     else:
         return {'error': f'Adoption not found with id {id}'}, 404
-    
-# expected data return = the adoption record that was just updated
-# will return the dog and adopter info nested in the adoption record
+
+# The above route will update the adoption with the id listed in the route
+# expected data return = the adoption record that was just updated, with the updated data + will return the dog and adopter info nested in the adoption record
 # If a dog_id or adopter_id that does not exist is used, a specific validation error will be thrown.
-# # If the adoption does not exist, the returned data will be "Adoption not found with id {id}"
+# If the adoption does not exist, the returned data will be "Adoption not found with id {id}"
--- src/controllers/auth_controller.py
+++ src/controllers/auth_controller.py
@@ -8,42 +8,57 @@ from datetime import timedelta
 
 auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-@auth_bp.route('/register', methods=['POST'])
+@auth_bp.route('/register', methods=['POST']) # Method = POST
 def auth_register():
     try:
-        body_data = request.get_json()
-
-        # Create a new User model instance from the user info
+        # expected input:
+        # {
+        #   "full_name" = "Jane Doe"
+        #   "email" = "user@email.com"
+        #   "password" = "user1pw"
+        # }
+        body_data = request.get_json() # Checks for body data inputted by user
         user = User()
         user.full_name = body_data.get('full_name')
         user.email = body_data.get('email')
         if body_data.get('password'):
-            user.password = bcrypt.generate_password_hash(body_data.get('password')).decode('utf-8')
+            user.password = bcrypt.generate_password_hash(body_data.get('password')).decode('utf-8') # encrypts the password so that it is not readable in the database
+        db.session.add(user) # Add the user to the session
+        db.session.commit() # Commit to add the user to the database
+        return user_schema.dump(user), 201 # Respond to the client
 
-        # Add the user to the session
-        db.session.add(user)
-        
-        # Commit to add the user to the database
-        db.session.commit()
-
-        # Respond to the client
-        return user_schema.dump(user), 201
-    
     except IntegrityError as err:
         if err.orig.pgcode == errorcodes.UNIQUE_VIOLATION:
             return {'error': 'Email address already in use'}, 409
         if err.orig.pgcode == errorcodes.NOT_NULL_VIOLATION:
             return {'error': f'The {err.orig.diag.column_name} is required'}, 409
+        
+# The above route creates a new User model instance from the user info
+# expected data return = the new user's information that was just created
+# If a user attemps to create a user with an email that has already been used, a Unique Violation error will be thrown.
+# If the user forgets to enter the name, email or password, a Not Null Violation error will be thrown.
 
-@auth_bp.route('/login', methods=['POST'])
+@auth_bp.route('/login', methods=['POST']) # Method = POST
 def auth_login():
-    body_data = request.get_json()
+    body_data = request.get_json() # Checks for body data inputted by user
+    # expected input:
+        # {
+        #   "email" = "user@email.com"
+        #   "password" = "user1pw"
+        # }
+
     # Find the user by email address
     stmt = db.select(User).filter_by(email=body_data.get('email'))
     user = db.session.scalar(stmt)
+
     # If user exists and password is correct
     if user and bcrypt.check_password_hash(user.password, body_data.get('password')):
-        token = create_access_token(identity=str(user.id), expires_delta=timedelta(days=1))
+        token = create_access_token(identity=str(user.id), expires_delta=timedelta(days=1)) # this token will expire in 1 day.
         return { 'email': user.email, 'token': token, 'is_employee': user.is_employee }
     else:
         return { 'error': 'Invalid email or password' }, 401
+    
+# The above route is used for a user to log in and receive their access token.
+# expected data return = email, token and "is_employee" status of user that is now logged in.
+# if the user enters an invalid email or password, an error will be thrown.
+    
